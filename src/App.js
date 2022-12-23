cb02ba2cf74b7b3feaf31adfbd2c098b56cb4484
aknous <aknous@pepperdash.com>
2022-12-22 23:55:56 -0500

    fixes

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Stats from './Views/Stats/Stats'
 import Players from './Views/Players/PlayerTable'
 import Results from './Views/Results/Results.js'
 import Dashboard from './Views/Dashboard/Dashboard'
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate, BrowserRouter } from 'react-router-dom';
 import axios from 'axios'
 
 function App() {
@@ -30,26 +30,28 @@ function App() {
   }, [])
   return (
     <div className='bg-slate-200'>
-      <div>
-        <Navbar />
-        <GameBar />
-      </div>
-      <div>
-        <Routes>
-          <Route exact path='/' element={<Navigate to='/dashboard'/>} />
-          <Route exact path='/dashboard' element={<Dashboard />} />
-          <Route exact path='/players' element={<Players />} />
-          <Route exact path='/stats' element={<Stats />} />
-          <Route exact path='/teams' element={<TeamGrid />} />
-          <Route exact path='/results' element={<Results />} />
-          <Route path='/team/:id' element={<Team />} />
-          <Route path='/players/:id' element={<Player />} />
-        </Routes>
-      </div>
-      <div className='w-full h-6'></div>
-      <div className='flex justify-end'>
-        <span className='text-slate-400 text-sm font-light mr-8 my-2'> floosball v{appVer}</span>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Navbar />
+          <GameBar />
+        </div>
+        <div>
+          <Routes>
+            <Route exact path='/' element={<Navigate to='/dashboard'/>} />
+            <Route exact path='/dashboard' element={<Dashboard />} />
+            <Route exact path='/players' element={<Players />} />
+            <Route exact path='/stats' element={<Stats />} />
+            <Route exact path='/teams' element={<TeamGrid />} />
+            <Route exact path='/results' element={<Results />} />
+            <Route path='/team/:id' element={<Team />} />
+            <Route path='/players/:id' element={<Player />} />
+          </Routes>
+        </div>
+        <div className='w-full h-6'></div>
+        <div className='flex justify-end'>
+          <span className='text-slate-400 text-sm font-light mr-8 my-2'> floosball v{appVer}</span>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
